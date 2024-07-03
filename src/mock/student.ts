7f474c4477b17969b90c00267afae68de542219b
Socn <xxy780910@gmail.com>
2024-07-04 06:06:53 +0800

    feat: implemented random student subjects generation feat: implemented searching student by name and id

--- src/mock/student.ts
+++ src/mock/student.ts
@@ -1,4 +1,5 @@
 import { defineFakeRoute } from 'vite-plugin-fake-server/client'
+import { shuffle } from 'lodash-es'
 import type { Student } from '@/interfaces/student'
 import randomGrade from '@/utils/randomGrade'
 
@@ -7,18 +8,20 @@ const studentList: Array<Student> = []
 
 function initStudentList() {
   for (let i = 1; i <= studentNum; i++) {
+    const chose2 = Math.floor(Math.random() * 2)
+    const chose4 = shuffle([randomGrade(60, 100), randomGrade(60, 100), -1, -1])
     const stu: Student = {
-      name: i.toString(),
-      id: i.toString(),
+      name: (i % 400).toString(),
+      id: (i + 302111000).toString(),
       chinese: randomGrade(50, 150),
       math: randomGrade(50, 150),
       english: randomGrade(50, 150),
-      physics: randomGrade(60, 100),
-      history: randomGrade(40, 100),
-      chemistry: randomGrade(60, 100),
-      biology: randomGrade(60, 100),
-      geography: randomGrade(60, 100),
-      politics: randomGrade(60, 100),
+      physics: chose2 === 0 ? randomGrade(60, 100) : -1,
+      history: chose2 === 1 ? randomGrade(40, 100) : -1,
+      chemistry: chose4[0],
+      biology: chose4[1],
+      geography: chose4[2],
+      politics: chose4[3],
     }
     studentList.push(stu)
   }
@@ -28,13 +31,19 @@ export default defineFakeRoute([
   {
     url: '/mock/student',
     method: 'get',
-    response: () => {
+    response: (request) => {
       if (studentList.length === 0) {
         initStudentList()
       }
+      const retList = studentList.filter((stu) => {
+        if (request.query.name === '' && request.query.id === '') return true
+        if (request.query.name === '') return stu.id === request.query.id
+        if (request.query.id === '') return stu.name === request.query.name
+        return (stu.id === request.query.id && stu.name === request.query.name)
+      })
       return {
         success: true,
-        students: studentList,
+        students: retList,
       }
     },
   },
