9dbadca9beb5a40cef123265c4f30ec886b249e8
Socn <xxy780910@gmail.com>
2024-07-04 06:04:25 +0800

    fix: sub array wont exceed its original length anymore

--- src/utils/subArray.ts
+++ src/utils/subArray.ts
@@ -1,6 +1,6 @@
 export function getSubArray(array: Array<any>, start: number, end: number) {
   const ret: Array<any> = []
-  for (let i = start; i < end; i++) {
+  for (let i = start; i < Math.min(array.length, end); i++) {
     ret.push(array[i])
   }
   return ret
